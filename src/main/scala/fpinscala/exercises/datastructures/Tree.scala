12377a8d9c2e34d2d61b66cbd563281361ecbc3b
Andrea Cecchini <andrea.cecchini13@studio.unibo.it>
2026-06-05 12:47:24 +0200

    feat: Tree exercises

--- src/main/scala/fpinscala/exercises/datastructures/Tree.scala
+++ src/main/scala/fpinscala/exercises/datastructures/Tree.scala
@@ -5,29 +5,45 @@ enum Tree[+A]:
   case Branch(left: Tree[A], right: Tree[A])
 
   def size: Int = this match
-    case Leaf(_) => 1
+    case Leaf(_)      => 1
     case Branch(l, r) => 1 + l.size + r.size
 
-  def depth: Int = ???
+  def depth: Int = this match
+    case Leaf(a)      => 0
+    case Branch(l, r) => 1 + math.max(l.depth, r.depth)
 
-  def map[B](f: A => B): Tree[B] = ???
+  def map[B](f: A => B): Tree[B] = this match
+    case Leaf(a)      => Leaf(f(a))
+    case Branch(l, r) => Branch(l.map(f), r.map(f))
 
-  def fold[B](f: A => B, g: (B,B) => B): B = ???
+  def fold[B](f: A => B, g: (B,B) => B): B = this match
+    case Leaf(a)        => f(a)
+    case Branch(l, r)   => g(l.fold(f, g), r.fold(f, g))
   
-  def sizeViaFold: Int = ???
+  def sizeViaFold: Int = 
+    fold(_ => 1, 1 + _ + _)
   
-  def depthViaFold: Int = ???
+  def depthViaFold: Int = 
+    fold(_ => 0, 1 + math.max(_, _))
   
-  def mapViaFold[B](f: A => B): Tree[B] = ???
+  def mapViaFold[B](f: A => B): Tree[B] = 
+    fold(a => Leaf(f(a)), Branch(_, _))
 
 object Tree:
 
   def size[A](t: Tree[A]): Int = t match
-    case Leaf(_) => 1
-    case Branch(l,r) => 1 + size(l) + size(r)
+    case Leaf(_)      => 1
+    case Branch(l,r)  => 1 + size(l) + size(r)
 
-  extension (t: Tree[Int]) def firstPositive: Int = ???
-
-  extension (t: Tree[Int]) def maximum: Int = ???
-
-  extension (t: Tree[Int]) def maximumViaFold: Int = ???
+  extension (t: Tree[Int]) def firstPositive: Int = t match
+    case Leaf(a) => a
+    case Branch(l, r) => l.firstPositive match
+      case i if i >= 0  => i
+      case _            => r.firstPositive 
+      
+  extension (t: Tree[Int]) def maximum: Int = t match
+    case Leaf(a) => a 
+    case Branch(l, r) => math.max(l.maximum, r.maximum)
+  
+  extension (t: Tree[Int]) def maximumViaFold: Int = 
+    t.fold(identity, math.max(_, _))
